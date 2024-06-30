e9649bf561127d5e2306295c47b6dc2c30ed01fe
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2024-06-30 18:55:30 -0500

    feat: added base & product aggregate

---
+++ si729ebu202210749/src/main/java/com/isa/platform/u202210749/Si729ebu202210749Application.java
@@ -0,0 +1,13 @@
+package com.isa.platform.u202210749;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729ebu202210749Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si729ebu202210749Application.class, args);
+    }
+
+}
